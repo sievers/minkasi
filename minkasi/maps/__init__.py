7aa614a1c67c47bea52b40093669c48a68b0e7a8
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-07-03 18:26:15 -0400

    Added some top level imports and made a general map type for typehints

---
+++ minkasi/maps/__init__.py
@@ -0,0 +1,5 @@
+from skymap import SkyMap, SkyMapCar, SkyMapCoarse, HealMap, SkyMapTwoRes
+from polmap import PolMap, HealPolMap
+from mapset import Mapset
+
+MapType = SkyMap | SkyMapTwoRes | PolMap
