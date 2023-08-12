8c8cbc69d172dafc465d73c6ab1fc20cded13048
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-08-13 00:07:13 +0200

    Simpler fake numba

--- minkasi/no_numba.py
+++ minkasi/no_numba.py
@@ -2,33 +2,21 @@
 Replacement for numba stuff when numba isn't installed.
 """
 import numpy as np
-import functools
 
 
 def prange(*args, **kwargs):
     return np.arange(*args, **kwargs)
 
 
-# Decorator sub based on https://github.com/ptooley/numbasub
-def optional_arg_decorator(fn):
-    @functools.wraps(fn)
-    def wrapped_decorator(*args, **kwargs):
-        # If no arguments were passed...
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return fn(args[0])
-        else:
+# https://stackoverflow.com/questions/69824610/dummy-function-decorator-with-arguments
+def __noop(*args0, **kwargs0):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
 
-            def real_decorator(decoratee):
-                return fn(decoratee, *args, **kwargs)
+        return inner
 
-            return real_decorator
-
-    return wrapped_decorator
-
-
-@optional_arg_decorator
-def __noop(func, *args, **kwargs):
-    return func
+    return wrapper
 
 
 autojit = __noop
