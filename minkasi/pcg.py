e090afcc9ddbecedc5168edc3c06bdc127f7c9bc
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-09-21 02:31:09 -0400

    Get feature parity between pcg routines and have run_pcg just wrap run_pcg_wprior to reduce repeated code

--- minkasi/pcg.py
+++ minkasi/pcg.py
@@ -1,13 +1,26 @@
+import sys
 import time
 from .maps import Mapset, SkyMap, PolMap
 from .tods import TodVec
 from .parallel import myrank
 
+if sys.version_info >= (3, 8):
+    from typing import Protocol, runtime_checkable
+else:
+    from typing_extensions import Protocol, runtime_checkable
 
-def run_pcg(
+
+@runtime_checkable
+class HasPrior(Protocol):
+    def apply_prior(self, x, Ax):
+        ...
+
+
+def run_pcg_wprior(
     b: Mapset,
     x0: Mapset,
     tods: TodVec,
+    prior: HasPrior | None = None,
     precon: Mapset | None = None,
     maxiter: int = 25,
     outroot: str = "map",
@@ -28,6 +41,15 @@ def run_pcg(
     (i.e. a matrix that specifies which pixel in the map was observed by each TOD data point).
     In this case x is the map we are solving for.
 
+    In the presence of a prior the least squares equation becomes:
+    chi^2 = (d-Am)^T N^-1 (d-Ax) + (p-x)^T Q^-1 (p-x).
+    Where p is the prior target for parameters, and Q is the variance.
+    So now the equation we want to solve is:
+    (A^T N-1 A + Q^-1)x = A^T N^-1 d + Q^-1 p.
+    For non-zero p, it is assumed you have done this already and that:
+    b=A^T N^-1 d + Q^-1 p.
+    So to have a prior apply a Q^-1 x to Ax every time we call Ax.
+
     Parameters
     ----------
     b : Mapset
@@ -39,6 +61,9 @@ def run_pcg(
     tods : TodVec
         The input TODs we want to make into maps.
         Note the noise should already been estimated and stored within each TOD object.
+    prior : HasPrior | None, default: None
+        Prior on the solution to the linear equation.
+        If not None then needs to implement the apply_prior method.
     precon : Mapset | None, default: None
         The preconditioner matrix applied to A to ensure faster convergence.
         1/hitsmap is a frequent selection.
@@ -48,7 +73,7 @@ def run_pcg(
     outroot : str, default: 'map'
         Root at which to save maps.
         Each saved iter will be saved at: outroot_{iter}{save_tail}
-    save_iters : list, deault: [-1]
+    save_iters : list, default: [-1]
         The iterations at which to save the result map.
         Default is to save only the last.
     save_ind : int, default: 0
@@ -101,10 +126,13 @@ def run_pcg(
                 print(i, zr, alpha, t2 - t1, t3 - t2, t3 - t1, nsamp / (t2 - t1) / 1e6)
             else:
                 print(i, zr, t2 - t1)
+            sys.stdout.flush()
         t1 = time.time()
 
         # Compute pAp
         Ap = tods.dot(p)
+        if prior is not None:
+            prior.apply_prior(p, Ap)
         t2 = time.time()
         pAp = p.dot(Ap)
 
@@ -165,95 +193,49 @@ def run_pcg(
     return x
 
 
-def run_pcg_wprior(
-    b,
-    x0,
-    tods,
-    prior=None,
-    precon=None,
-    maxiter=25,
-    outroot="map",
-    save_iters=[-1],
-    save_ind=0,
-    save_tail=".fits",
-):
-    # least squares equations in the presence of a prior - chi^2 = (d-Am)^T N^-1 (d-Am) + (p-m)^T Q^-1 (p-m)
-    # where p is the prior target for parameters, and Q is the variance.  The ensuing equations are
-    # (A^T N-1 A + Q^-1)m = A^T N^-1 d + Q^-1 p.  For non-zero p, it is assumed you have done this already and that
-    # b=A^T N^-1 d + Q^-1 p
-    # to have a prior then, whenever we call Ax, just a Q^-1 x to Ax.
-    t1 = time.time()
-    Ax = tods.dot(x0)
-    if not (prior is None):
-        # print('applying prior')
-        prior.apply_prior(x0, Ax)
-    try:
-        r = b.copy()
-        r.axpy(Ax, -1)
-    except:
-        r = b - Ax
-    if not (precon is None):
-        z = precon * r
-    else:
-        z = r.copy()
-    p = z.copy()
-    k = 0.0
-
-    zr = r.dot(z)
-    x = x0.copy()
-    t2 = time.time()
-    for iter in range(maxiter):
-        if myrank == 0:
-            if iter > 0:
-                print(iter, zr, alpha, t2 - t1, t3 - t2, t3 - t1)
-            else:
-                print(iter, zr, t2 - t1)
-            sys.stdout.flush()
-        t1 = time.time()
-        Ap = tods.dot(p)
-        if not (prior is None):
-            # print('applying prior')
-            prior.apply_prior(p, Ap)
-        t2 = time.time()
-        pAp = p.dot(Ap)
-        alpha = zr / pAp
-        try:
-            x_new = x.copy()
-            x_new.axpy(p, alpha)
-        except:
-            x_new = x + p * alpha
-
-        try:
-            r_new = r.copy()
-            r_new.axpy(Ap, -alpha)
-        except:
-            r_new = r - Ap * alpha
-        if not (precon is None):
-            z_new = precon * r_new
-        else:
-            z_new = r_new.copy()
-        zr_new = r_new.dot(z_new)
-        beta = zr_new / zr
-        try:
-            p_new = z_new.copy()
-            p_new.axpy(p, beta)
-        except:
-            p_new = z_new + p * beta
-
-        p = p_new
-        z = z_new
-        r = r_new
-        zr = zr_new
-        x = x_new
-        t3 = time.time()
-        if iter in save_iters:
-            if myrank == 0:
-                x.maps[save_ind].write(outroot + "_" + repr(iter) + save_tail)
-
-    return x
+def run_pcg(
+    b: Mapset,
+    x0: Mapset,
+    tods: TodVec,
+    precon: Mapset | None = None,
+    maxiter: int = 25,
+    outroot: str = "map",
+    save_iters: list = [-1],
+    save_ind: int = 0,
+    save_tail: str = ".fits",
+    plot_iters: list = [],
+    plot_info: dict | None = None,
+    plot_ind: int = 0,
+) -> Mapset:
+    """
+    Wrapper function that just calls run_pcg_wprior with no prior.
+    This exists for convenience/compatibility reasons,
+    see the docstring of run_pcg_wprior for details on the params.
+    """
+    return run_pcg_wprior(
+        b=b,
+        x0=x0,
+        tods=tods,
+        prior=None,
+        precon=precon,
+        maxiter=maxiter,
+        outroot=outroot,
+        save_iters=save_iters,
+        save_ind=save_ind,
+        save_tail=save_tail,
+        plot_iters=plot_iters,
+        plot_info=plot_info,
+        plot_ind=plot_ind,
+    )
 
 
 class null_precon:
+    """
+    Seems to be a dummy class to store a preconditioner
+    that does nothing, no real reason to use this since you
+    can just pass in precon=None if you don't want to precondition.
+    """
+
     def __init__(self):
         self.isnull = True
 
