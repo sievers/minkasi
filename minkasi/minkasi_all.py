db0da7de3bdadf7abf218c7a81dc1aa57a40e019
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-09-21 13:51:45 -0400

    Fix relative imports

--- minkasi/minkasi_all.py
+++ minkasi/minkasi_all.py
@@ -5,22 +5,21 @@ Intended for supporting legacy code that expects this.
 Please don't use if you are writing new code.
 """
 
-from fitting.fit import *
-from fitting.models import *
-from fitting.power_spectrum import *
-from fitting.utils import *
-from map2tod import *
-from noise import *
-from parallel import *
-from pcg import *
-from pyregion_tools import *
-from smooth import *
-from timestream import *
-from tod2map import *
-from tods.core import *
-from tods.cuts import *
-from tods.io import *
-from tods.processing import *
-from tods.utils import *
-from utils import *
-from zernike import *
+from .fitting.core import *
+from .fitting.models import *
+from .fitting.power_spectrum import *
+from .map2tod import *
+from .noise import *
+from .parallel import *
+from .pcg import *
+from .pyregion_tools import *
+from .smooth import *
+from .timestream import *
+from .tod2map import *
+from .tods.core import *
+from .tods.cuts import *
+from .tods.io import *
+from .tods.processing import *
+from .tods.utils import *
+from .utils import *
+from .zernike import *
