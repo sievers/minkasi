45640cc1cc917511fd827e105ae07763dffa792f
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-09-23 11:23:10 -0400

    Bump version and add extra requirements

--- setup.py
+++ setup.py
@@ -8,28 +8,29 @@ from extension_helpers import add_openmp_flags_if_available
 from pathlib import Path
 
 
-minkasi_dir = Path(__file__).parent.joinpath('minkasi/lib')
+minkasi_dir = Path(__file__).parent.joinpath("minkasi/lib")
 
 
 def pkgconfig(package, kw):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    cmd = 'pkg-config --cflags --libs {}'.format(package)
+    flag_map = {"-I": "include_dirs", "-L": "library_dirs", "-l": "libraries"}
+    cmd = "pkg-config --cflags --libs {}".format(package)
     exitcode, output = subprocess.getstatusoutput(cmd)
     if exitcode != 0:
-        raise ValueError(
-            f"Failed to run command `{cmd}`:\n\n{output}\n")
+        raise ValueError(f"Failed to run command `{cmd}`:\n\n{output}\n")
     for token in output.strip().split():
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
     return kw
 
 
 def _add_common_ext_kwargs(cfg):
-    if get_compiler() in ('unix', 'mingw32'):
-        cfg['extra_compile_args'].extend([
-            '-pedantic',
-            '-Wno-newline-eof',
-            '-Wno-unused-const-variable',
-            ])
+    if get_compiler() in ("unix", "mingw32"):
+        cfg["extra_compile_args"].extend(
+            [
+                "-pedantic",
+                "-Wno-newline-eof",
+                "-Wno-unused-const-variable",
+            ]
+        )
     return cfg
 
 
@@ -37,41 +38,36 @@ def minkasi_extension():
     cfg = defaultdict(list)
 
     files = [
-        'minkasi.c',
-        ]
-    cfg['sources'].extend(
-            minkasi_dir.joinpath(x).as_posix() for x in files)
+        "minkasi.c",
+    ]
+    cfg["sources"].extend(minkasi_dir.joinpath(x).as_posix() for x in files)
 
     # external dependencies
-    cfg['libraries'].append('m')
+    cfg["libraries"].append("m")
     _add_common_ext_kwargs(cfg)
 
-    return Extension('minkasi.lib._libminkasi', **cfg)
+    return Extension("minkasi.lib._libminkasi", **cfg)
 
 
 def mkfftw_extension():
     cfg = defaultdict(list)
 
-    files = [
-        'mkfftw.c'
-        ]
-    cfg['sources'].extend(
-            minkasi_dir.joinpath(x).as_posix() for x in files)
+    files = ["mkfftw.c"]
+    cfg["sources"].extend(minkasi_dir.joinpath(x).as_posix() for x in files)
 
     # external dependencies
-    cfg['libraries'].append('m')
+    cfg["libraries"].append("m")
 
     # add fftw3 config
-    cfg = pkgconfig('fftw3', cfg)
+    cfg = pkgconfig("fftw3", cfg)
     # this only added the non-threaded libs, and we also need the threaded
     # ones
-    cfg['libraries'].extend(['fftw3_threads', 'fftw3f', 'fftw3f_threads'])
+    cfg["libraries"].extend(["fftw3_threads", "fftw3f", "fftw3f_threads"])
     _add_common_ext_kwargs(cfg)
-    return Extension('minkasi.lib._libmkfftw', **cfg)
+    return Extension("minkasi.lib._libmkfftw", **cfg)
 
 
 def get_extensions():
-
     extensions = [minkasi_extension(), mkfftw_extension()]
     for ext in extensions:
         add_openmp_flags_if_available(ext)
@@ -79,16 +75,19 @@ def get_extensions():
 
 
 setup(
-    name='minkasi',
-    version='1.0.0',
+    name="minkasi",
+    packages=["minkasi"],
+    version="2.0.0",
     install_requires=[
-        'requests',
-        'importlib-metadata',
-		'numpy',
-		'astropy',
-		'scipy'
+        "requests",
+        "importlib-metadata",
+        "numpy",
+        "astropy",
+        "scipy",
+        "pyregion",
     ],
-    packages=['minkasi'],
-    # package_data={"minkasi": ["*.so"]},
-    ext_modules = get_extensions()
+    extras_require={
+        "extras": ["qpoint", "numba", "healpy"],
+    },
+    ext_modules=get_extensions(),
 )
