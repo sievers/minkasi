498dadf5ad18d091ece59e4114fa45a0ccd7a42d
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-09-28 14:45:07 -0400

    fix: Get extension file suffix from sysconfig

--- minkasi/lib/_minkasi_init.py
+++ minkasi/lib/_minkasi_init.py
@@ -1,12 +1,13 @@
 import ctypes
+import sysconfig
 from pathlib import Path
 
+__all__ = ["libminkasi", "libmkfftw"]
 
-__all__ = ['libminkasi', 'libmkfftw']
+suffix = sysconfig.get_config_var("EXT_SUFFIX")
 
-
-_libminkasi_path = next(iter(Path(__file__).parent.glob("_libminkasi.*.so")))
+_libminkasi_path = Path(__file__).parent.joinpath("_libminkasi" + suffix)
 libminkasi = ctypes.cdll.LoadLibrary(_libminkasi_path.as_posix())
 
-_libmkfftw_path = next(iter(Path(__file__).parent.glob("_libmkfftw.*.so")))
+_libmkfftw_path = Path(__file__).parent.joinpath("_libmkfftw" + suffix)
 libmkfftw = ctypes.cdll.LoadLibrary(_libmkfftw_path.as_posix())
