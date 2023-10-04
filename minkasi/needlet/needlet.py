d418140d5cea2105e32323f55ad788f8ce302239
John Orlowski-Scherer <jorlo@nia-login06.scinet.local>
2023-10-04 15:23:56 -0400

    making wavskymap a subclass of skymap

--- minkasi/needlet/needlet.py
+++ minkasi/needlet/needlet.py
@@ -1,8 +1,12 @@
 from numba import jit
 
 from astropy.io import fits
+from astropy import wcs
 
 import numpy as np
+from numpy.typing import NDArray
+
+from typing import TYPE_CHECKING, Callable, List, Optional, Sequence, Tuple, Union
 
 from scipy.linalg import norm
 import scipy.integrate as integrate
@@ -19,15 +23,255 @@ from ..mapmaking.tod2map import (
 from ..parallel import comm, get_nthread, have_mpi, nproc
 from ..tods import Tod, TodVec
 from ..tools.fft import find_good_fft_lens
-
+from ..maps.skymap import SkyMap
 
 from pixell import enmap
 
 import matplotlib.pyplot as plt
 
 import copy
+import sys
 
-class WavSkyMap:
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+class WavSkyMap(SkyMap):
+    """
+    Wavelet based SkyMap. 
+    Subclass of SkyMap. See Skymap for full documentation.    
+ 
+
+    Attributes
+    ----------
+    filters : NDArray[np.floating]
+        An array specifying the needlet filter response from needlet class
+    nfilt : int
+        Number of needlet filters
+    wmap : NDArray[np.floating]
+        Array of wavelet coefficients. Can also be thought of as the wavelet space map.
+    """
+
+    def __init__(
+        self,
+        filters: NDArray[np.floating],
+        lims: Union[list[float], NDArray[np.floating]],
+        pixsize: float,
+        proj: str = "CAR",
+        pad: int = 2,
+        square: bool = False,
+        multiple: Union[int, bool] = False,
+        primes: Union[list[int], None] = None,
+        cosdec: Union[float, None] = None,
+        nx: Union[int, None] = None,
+        ny: Union[int, None] = None,
+        mywcs: Union[wcs.WCS, None] = None,
+        tag: str = "ipix",
+        purge_pixellization: bool = False,
+        ref_equ: bool = False,
+    ):
+        """
+        Initialize the SkyMap.
+
+        Parameters
+        ----------
+        filters: NDArray[np.floating]
+            An array specifying the needlet filter response from needlet class
+        See SkyMap docstring for remainer
+        """
+        self.filters = filters
+        super().__init__(lims, pixsize, proj, pad, square, multiple, primes, cosdec, nx, ny, mywcs, tag, purge_pixellization, ref_equ)
+        self.nfilt = len(self.filters)
+        self.wmap = np.zeros([self.nfilt, self.nx, self.ny])
+
+    def axpy(self,map,a):
+        """
+        Apply a*x + y to wavelet map. 
+   
+        Parameters
+        ----------
+        map : WavSkyMap
+             The map to act as y
+
+        a : float
+            Number to mutiply the map by.
+        """
+        self.wmap[:]=self.wmap[:]+a*map.wmap[:]
+
+    def clear(self):
+        """
+        Zero out the wmap.
+        """
+        self.wmap[:] = 0
+
+    def clear_real_map(self):
+        """
+        Zero out the realspace map.
+        """
+        self.map[:] = 0
+
+    #def assign(self, arr: NDArray[np.floating]):
+        """
+        Assign new values to wmap.
+
+        Parameters
+        ----------
+        arr : NDArray[np.floating]
+            Array of values to assign to wmap.
+            Should be the same shape as self.wmap.
+        """
+    #    assert arr.shape[0] == self.nfilt
+    #    assert arr.shape[1] == self.nx
+    #    assert arr.shape[2] == self.ny
+
+    #    self.map[:] = arr
+
+    def assign_real_map(self, arr: NDArray[np.floating]):
+        """
+        Assign new values to realspace map.
+
+        Parameters
+        ----------
+        arr : NDArray[np.floating]
+            Array of values to assign to map.
+            Should be the same shape as self.map.
+        """
+        assert arr.shape[0] == self.nx
+        assert arr.shape[1] == self.ny
+       
+        self.map[:] = arr            
+      
+    def map2tod(
+        self,
+        tod: "Tod",
+        dat: NDArray[np.floating],
+        do_add: bool = True,
+        do_omp: bool = True,
+    ):
+        """
+        Project a wavelet map into a tod, adding or replacing the map contents. 
+        First converts from wavelet map to realspace map, then from realspace map to tod.
+
+        Parameters
+        ----------
+        tod : Tod
+            Tod object, used to get pixellization.
+        dat : NDArray[np.floating]
+            Array to put tod data into.
+            Shape should be (ndet, ndata).
+        do_add : bool, default: True
+            If True add the projected map to dat.
+            If False replace dat with it.
+        do_omp : bool, default: False
+            Use omp to parallelize  
+        """        
+        ipix=self.get_pix(tod)
+        self.map = np.squeeze(wav2map_real(self.wmap, self.filters), axis = 0) #Right now let's restrict ourself to 1 freqency input maps, so we need to squeeze down the dummy axis added by map2wav_real 
+        map2tod(dat,self.map,ipix,do_add,do_omp)
+
+    def tod2map(
+        self,
+        tod: "Tod",
+        dat: NDArray[np.floating],
+        do_add: bool = True,
+        do_omp: bool = True,
+    ):
+        """
+        Project a tod into the wmap. Frist projects tod onto real space map, then converts that to wmap.
+
+        Parameters
+        ----------
+        tod : Tod
+            Tod object, used to get pixellization.
+        dat : NDArray[np.floating]
+            Array to pull tod data from.
+            Shape should be (ndet, ndata).
+        do_add : bool, default: True.
+            If True add the projected map to this map.
+            If False replace this map with it.
+        do_omp : bool, default: True
+            Use omp to parallelize.
+        """
+        if dat is None:
+            dat=tod.get_data()
+        if do_add==False:
+            self.clear()
+        ipix=self.get_pix(tod)
+
+        if not(self.caches is None):
+            tod2map_cached(self.caches,dat,ipix)
+
+        tod2map_simple(self.map,dat,ipix)
+        self.wmap = np.squeeze(map2wav_real(self.map, self.filters), axis = 0) #Right now let's restrict ourself to 1 freqency input maps, so we need to squeeze down the dummy axis added by map2wav_real
+
+        if self.purge_pixellization:
+            tod.clear_saved_pix(self.tag)
+
+    def dot(self, map: Self) -> float:
+        """
+        Take dot product of this wmap with another.
+
+        Parameters
+        ----------
+        map : WavSkyMap
+            Map to take dot product with.
+
+        Returns
+        -------
+        tot : float
+            The dot product
+        """
+        tot=np.sum(self.wmap*map.wmap)
+        return tot
+
+    def write(self, fname: str = "map.fits"):
+        """
+        Write map to a FITs file.
+
+        Parameters
+        ----------
+        fname : str, default: 'map.fits'
+            The path to save the map to.
+        """
+        self.map =  np.squeeze(wav2map_real(self.wmap, self.filters), axis = 0)
+        header=self.wcs.to_header()
+        if True: #try a patch to fix the wcs xxx
+            tmp=self.map.transpose().copy()
+            hdu=fits.PrimaryHDU(tmp,header=header)
+        else:
+            hdu=fits.PrimaryHDU(self.map,header=header)
+        try:
+            hdu.writeto(fname,overwrite=True)
+        except:
+            hdu.writeto(fname,clobber=True)
+    def __mul__(self, map: Self) -> Self:
+        """
+        Multiply this map with another.
+
+        Parameters
+        ----------
+        map : WavSkyMap
+            Map to multiply by.
+
+        Returns
+        -------
+        new_map : WavSkylMap
+            The result of the multiplication.
+        """
+        new_map=map.copy()
+        new_map.wmap[:]=self.wmap[:]*map.wmap[:]
+        return new_map
+   
+    #def invert(self):
+        """
+        Invert the map. Note that the inverted map is stored in self.map.
+        """
+    #    mask = np.abs(self.wmap) > 0
+    #    self.wmap[mask] = 1.0 / self.wmap[mask]
+
+
+class _WavSkyMap:
     def __init__(self,lims, filters,pixsize=0,proj='CAR',pad=2, square=False, multiple=False, primes=None,cosdec=None,nx=None,ny=None,mywcs=None,tag='ipix',purge_pixellization=False,ref_equ=False):
         if mywcs is None:
             assert(pixsize!=0) #we had better have a pixel size if we don't have an incoming WCS that contains it
