5c961e6b47a9223b90c64bddbe24d1e6da99caeb
John Orlowski-Scherer <jorlo@nia-login06.scinet.local>
2023-10-24 10:48:39 -0400

    re-enabling dim check

--- minkasi/needlet/needlet.py
+++ minkasi/needlet/needlet.py
@@ -543,12 +543,12 @@ def map2wav_real(imaps, filters):
         multimap of wavelet coefficients
     """
 
-    #if len(imaps.shape) == 2:
-    #    imaps = np.expand_dims(imaps, axis=0)
+    if len(imaps.shape) == 2:
+        imaps = np.expand_dims(imaps, axis=0)
 
-    #elif len(imaps.shape) != 3:
-    #    print("Error: input map must have dim = 2 or 3")
-    #    return
+    elif len(imaps.shape) != 3:
+        print("Error: input map must have dim = 2 or 3")
+        return
     #print(imaps.shape)
     filtered_slices_real = []
 
@@ -573,12 +573,12 @@ def map2wav_real(imaps, filters):
 
 
 def wav2map_real(wav_mapset, filters):
-    #if len(wav_mapset.shape) == 3:
-    #    wav_mapset = np.expand_dims(wav_mapset, axis=0)
+    if len(wav_mapset.shape) == 3:
+        wav_mapset = np.expand_dims(wav_mapset, axis=0)
 
-    #elif len(wav_mapset.shape) != 4:
-    #    print("Error: input wave mapset must have dim = 3 or 4")
-    #    return
+    elif len(wav_mapset.shape) != 4:
+        print("Error: input wave mapset must have dim = 3 or 4")
+        return
 
     #npix = wav_mapset.shape[-2] * wav_mapset.shape[-1]
     #weights = np.array([np.sum(f**2) / npix for f in filters])
@@ -588,7 +588,6 @@ def wav2map_real(wav_mapset, filters):
         fourier_boxes = []
         for b in wav_mapset[nu]:
             fourier_boxes.append(np.fft.fftn(np.fft.fftshift(b)))
-e
         #npix_f = fourier_boxes[0].shape[-1] * fourier_boxes[0].shape[-2]
 
         back_transform = np.zeros_like(fourier_boxes[0])
